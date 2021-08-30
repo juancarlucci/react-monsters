c31c456cdba0e201c96122bf95ac45151e09e9c2
juancarlucci <juancarloscollins@gmail.com>
2021-08-30 09:50:23 +0200

    Updates

---
+++ precache-manifest.0d465e2c7a8cf494259d25202d9811ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffaf40f08b2e2adabe5b485e8b88e25",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "0bb36643afa3305719fb",
+    "url": "/react-monsters/static/css/main.e9966077.chunk.css"
+  },
+  {
+    "revision": "b260d2dd141c0c27de97",
+    "url": "/react-monsters/static/js/2.c877fdbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.c877fdbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb36643afa3305719fb",
+    "url": "/react-monsters/static/js/main.2a47f7c2.chunk.js"
+  },
+  {
+    "revision": "a488bef04a42031c06ca",
+    "url": "/react-monsters/static/js/runtime-main.97ee3d9b.js"
+  }
+]);
